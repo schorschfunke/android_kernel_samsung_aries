59a0a7bafc4ef36df78c62ee10fafe9c8046cad1
fishears <f1she4rs@gmail.com>
2012-12-30 10:37:01 +0000

    add 1400 OC

--- arch/arm/mach-s5pv210/mach-aries.c
+++ arch/arm/mach-s5pv210/mach-aries.c
@@ -470,8 +470,11 @@ static struct s5p_media_device aries_media_devs[] = {
 #ifdef CONFIG_CPU_FREQ
 static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 	{
-
-		.freq	= 1200000,
+		.freq	= 1400000,
+		.varm	= 1350000,
+		.vint	= 1150000,
+	}, {
+		.freq	= 1000000,
 		.varm	= 1275000,
 		.vint	= 1100000,
 	}, {
@@ -479,12 +482,10 @@ static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
 		.varm	= 1275000,
 		.vint	= 1100000,
 	}, {
-
 		.freq	=  800000,
 		.varm	= 1200000,
 		.vint	= 1100000,
 	}, {
-
 		.freq	=  400000,
 		.varm	= 1050000,
 		.vint	= 1100000,
